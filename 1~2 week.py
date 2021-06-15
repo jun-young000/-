563c2081ae7ff399176be9b78a11fe4588da105d
jun-young000 <83482083+jun-young000@users.noreply.github.com>
2021-06-15 23:09:20 +0900

    Create 1~2 week.py

---
+++ 1~2 week.py
@@ -0,0 +1,36 @@
+"""
+As you can see, the code is broken.
+Create the missing functions, use default arguments.
+Sometimes you have to use 'return' and sometimes you dont.
+Start by creating the functions
+"""
+def is_on_list(week,day):
+    if day in week:
+        return True
+    else:
+        return False
+        
+def get_x(week,day):
+    return week[day]
+
+def add_x(week,day):
+    return week.append(day)
+
+def remove_x(week,day):
+    week.remove(day)
+# \/\/\/\/\/\/\  DO NOT TOUCH AREA  \/\/\/\/\/\/\ #
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+print("Is Wed on 'days' list?", is_on_list(days, "Wed"))
+
+print("The fourth item in 'days' is:", get_x(days, 3))
+
+add_x(days, "Sat")
+print(days)
+
+remove_x(days, "Mon")
+print(days)
+
+
+# /\/\/\/\/\/\/\ END DO NOT TOUCH AREA /\/\/\/\/\/\/\ #
